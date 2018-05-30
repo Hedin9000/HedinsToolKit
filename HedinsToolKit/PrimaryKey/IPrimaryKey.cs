7167edc6a544d1ab36c596c411aaf4bdb53bd365
Hedin9000 <hedin495@gmail.com>
2018-05-30 14:57:29 +0300

    Add IPrimaryKey interface and Extensions.

---
+++ HedinsToolKit/PrimaryKey/IPrimaryKey.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HedinsToolKit.PrimaryKey
+{
+    public interface IPrimaryKey<out TKey>
+    where TKey : IEquatable<TKey>
+    {
+        TKey Id { get; }
+    }
+}
---
+++ HedinsToolKit/PrimaryKey/PrimaryKeyExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HedinsToolKit.PrimaryKey
+{
+    public static class PrimaryKeyExtensions
+    {
+        /// <summary>
+        /// Find item by id. If item is not exist - return null;
+        /// </summary>
+        /// <typeparam name="TEntity">Type of item</typeparam>
+        /// <typeparam name="TKey">Type of key</typeparam>
+        /// <param name="list">Source collection</param>
+        /// <param name="id">Key</param>
+        /// <returns>Item (or null)</returns>
+        public static TEntity GetById<TEntity, TKey>(this IEnumerable<TEntity> list,TKey id )
+        where TEntity : IPrimaryKey<TKey> 
+        where TKey : IEquatable<TKey>
+        {
+            var resultList = from item in list
+                             where item.Id.Equals(id)
+                             select item;
+
+            return resultList.FirstOrDefault();
+        }
+    }
+
+
+}
